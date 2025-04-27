5fb81b124795bf9e762dbafb34088e566ef5ec7c
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-04-26 23:50:42 -0600

    starting new project service_soap

---
+++ src/main/java/com/service_soap/ServiceSoapApplication.java
@@ -0,0 +1,13 @@
+package com.service_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSoapApplication.class, args);
+	}
+
+}
